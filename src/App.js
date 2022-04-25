340bd9935085fa9371cb34a2dda7bdbae232dd14
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-04-25 13:20:06 -0500

    correçao no app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Album from './pages/Album';
+import AlbumId from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
@@ -19,7 +19,7 @@ class App extends React.Component {
             render={ (props) => (<Login { ...props } />) }
           />
           <Route exact path="/search" render={ () => <Search /> } />
-          <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+          <Route exact path="/album/:id" render={ (props) => <AlbumId { ...props } /> } />
           <Route exact path="/favorites" render={ () => <Favorites /> } />
           <Route exact path="/profile" render={ () => <Profile /> } />
           <Route exact path="/profile/edit" render={ () => <ProfileEdit /> } />
